e185cbb29187e7423a16ee63067b537309b34f98
choding91 <whdlsrjf91@naver.com>
2022-11-20 20:33:49 +0900

    로그아웃 기능 구현 완료

--- users/urls.py
+++ users/urls.py
@@ -7,6 +7,7 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path("signup/", views.SignupView.as_view(), name="signup_view"),
+    path("logout/", views.LogoutView.as_view(), name="logout_view"),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from users.serializers import UserSerializer
+from django.contrib.auth import logout
 
 
 class SignupView(APIView):
@@ -12,3 +13,23 @@ class SignupView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+    # # 회원탈퇴
+    # def delete(self, request, **kwargs):
+    #     if kwargs.get('user_id') is None:
+    #         return Response("invalid request", status=status.HTTP_400_BAD_REQUEST)
+    #     else:
+    #         user_id = kwargs.get('user_id')
+    #         user_object = User.objects.get(id=user_id)
+    #         user_object.delete()
+    #         return Response("delete ok", status=status.HTTP_200_OK)
+
+
+class LogoutView(APIView):
+    def delete(self, request):
+        if request.user.is_authenticated:
+            logout(request)
+            return Response({'message': 'logout complete'}, status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response({'message': 'invalid request'}, status=status.HTTP_200_OK)
